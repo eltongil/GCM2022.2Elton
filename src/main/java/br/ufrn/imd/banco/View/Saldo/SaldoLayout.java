172e6abdb46bbb333d1a65e0eba7b78ce8426cc8
Elton Moura <eltongilx@gmail.com>
2022-05-25 16:02:40 -0300

    Correção do erro de fire dos botões

--- src/main/java/br/ufrn/imd/banco/View/Saldo/SaldoLayout.java
+++ src/main/java/br/ufrn/imd/banco/View/Saldo/SaldoLayout.java
@@ -6,9 +6,10 @@ import javafx.scene.layout.VBox;
 public class SaldoLayout{
     private static VBox singleton = setColuna();
     public static VBox getInstance() {return singleton;}
+    private static EscolherConta seletor = new EscolherConta();
     protected static VBox setColuna(){
         VBox colunaSaldo = new VBox();
-        colunaSaldo.getChildren().add(EscolherConta.getInstance());
+        colunaSaldo.getChildren().add(seletor.getContas());
         colunaSaldo.getChildren().add(SaldoBotao.getInstance());
         colunaSaldo.getChildren().add(SaldoText.getInstance());
         return colunaSaldo;
